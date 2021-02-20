290e08f285c84b1c5497bdf8638eadeeab167c16
tedwang <tedwang@sina.cn>
2021-02-20 15:10:08 +0800

    0.79 优化 节点update脚本      新增 Web测试示例:发送邮件

--- Utils/RPC/update.bat
+++ Utils/RPC/update.bat
@@ -1,7 +1,8 @@
 title=update
 rem update&&restart
 taskkill /F /FI "WINDOWTITLE eq Node Server..." /FI "WINDOWTITLE ne update"
-git pull origin master >>update_log.txt
 rem timeout /T 10 /NOBREAK
+git pull origin master >>update_log.txt 2>&1
+timeout /T 10 /NOBREAK
 start "" cmd  /k call startServer.bat
 exit
