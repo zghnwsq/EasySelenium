67b62e894786740c98ebd5e79e0b4dbe9efd3b02
tedwang <tedwang@sina.cn>
2021-02-21 11:35:39 +0800

    0.791 优化 节点update脚本,修改TestMail

--- TestCases/Demo/TestMail.py
+++ TestCases/Demo/TestMail.py
@@ -16,7 +16,7 @@ file_path = os.path.join(Settings.BASE_DIR, 'DS', 'TestMail', 'TestMail.yaml')
 class TestMail:
 
     def setup_method(self):
-        self.driver = chrome(path=Settings.DRIVER_PATH['chrome'], user_dir=r'C:\Users\tedwa\AppData\Local\Google\UserData')
+        self.driver = chrome(path=Settings.DRIVER_PATH['chrome'])
         self.driver.delete_all_cookies()
         self.log = logger('info')
         self.el = Element(self.driver, self.log)
--- Utils/RPC/update.bat
+++ Utils/RPC/update.bat
@@ -2,6 +2,7 @@ title=update
 rem update&&restart
 taskkill /F /FI "WINDOWTITLE eq Node Server..." /FI "WINDOWTITLE ne update"
 rem timeout /T 10 /NOBREAK
+echo =====%date% %time%===== >> update_log.txt
 git pull origin master >>update_log.txt 2>&1
 timeout /T 10 /NOBREAK
 start "" cmd  /k call startServer.bat
