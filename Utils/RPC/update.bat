c880b87ac003a78c482e16a35323f47174713ecc
tedwang <tedwang@sina.cn>
2021-03-25 13:59:04 +0800

    0.831 优化 updata.bat中git更新采用强制覆盖本机方法;解决Utils.Database包互相导入名称冲突问题

--- Utils/RPC/update.bat
+++ Utils/RPC/update.bat
@@ -3,7 +3,8 @@ rem update&&restart
 taskkill /F /FI "WINDOWTITLE eq Node Server..." /FI "WINDOWTITLE ne update"
 rem timeout /T 10 /NOBREAK
 echo =====%date% %time%===== >> update_log.txt
-git pull origin master >>update_log.txt 2>&1
+rem git pull origin master >>update_log.txt 2>&1
+git fetch --all && git reset --hard origin/master && git pull >>update_log.txt 2>&1
 timeout /T 10 /NOBREAK
 start "" cmd  /k call startServer.bat
 exit
